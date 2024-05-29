05c03c685a550cd5736690cd5f3dc9a092530dc6
Pengchong Jin <pengchong@google.com>
2024-05-29 16:25:29 +0000

    Expose logits from the sampler.

--- gemma/model_xla.py
+++ gemma/model_xla.py
@@ -48,7 +48,7 @@ class Sampler(nn.Module):
         top_ps: torch.Tensor,
         top_ks: torch.Tensor,
         embedding_bias: Optional[torch.Tensor] = None,
-    ) -> torch.Tensor:
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
         # Select the last element for each sequence.
         # (batch_size, input_len, hidden_size) -> (batch_size, hidden_size)
         hidden_states = hidden_states.index_select(
@@ -71,7 +71,7 @@ class Sampler(nn.Module):
             logits += embedding_bias
 
         if temperatures is None:
-            return torch.argmax(logits, dim=-1).squeeze(dim=-1)
+            return torch.argmax(logits, dim=-1).squeeze(dim=-1), logits
 
         # Apply temperature scaling.
         logits.div_(temperatures.unsqueeze(dim=1))
@@ -100,7 +100,7 @@ class Sampler(nn.Module):
         next_token_ids = torch.multinomial(probs,
                                            num_samples=1,
                                            replacement=True).squeeze(dim=-1)
-        return next_token_ids
+        return next_token_ids, logits
 
 
 def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
@@ -508,7 +508,7 @@ class GemmaForCausalLM(nn.Module):
         if self.config.quant:
             embedder_weight = (
                 embedder_weight * self.embedder.weight_scaler.unsqueeze(-1))
-        next_tokens = self.sampler(
+        next_tokens, _ = self.sampler(
             embedding=embedder_weight,
             hidden_states=hidden_states,
             output_positions=output_positions,
