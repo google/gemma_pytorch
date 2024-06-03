86a5c2a2d96b50536e7816e5394ee13af25f9043
Michelle Casbon <michellecasbon@google.com>
2024-06-03 11:40:24 +0000

    Modify SentencePiece function calls.

--- gemma/tokenizer.py
+++ gemma/tokenizer.py
@@ -14,7 +14,7 @@
 import os
 from typing import List, Optional
 
-from sentencepiece import SentencePieceProcessor
+import sentencepiece
 
 
 class Tokenizer:
@@ -22,19 +22,19 @@ class Tokenizer:
     def __init__(self, model_path: Optional[str]):
         # Reload tokenizer.
         assert os.path.isfile(model_path), model_path
-        self.sp_model = SentencePieceProcessor(model_file=model_path)
+        self.sp_model = sentencepiece.SentencePieceProcessor()
+        self.sp_model.Load(model_path)
 
         # BOS / EOS token IDs.
-        self.n_words: int = self.sp_model.vocab_size()
+        self.n_words: int = self.sp_model.GetPieceSize()
         self.bos_id: int = self.sp_model.bos_id()
         self.eos_id: int = self.sp_model.eos_id()
         self.pad_id: int = self.sp_model.pad_id()
-        assert self.sp_model.vocab_size() == self.sp_model.get_piece_size()
 
     def encode(self, s: str, bos: bool = True, eos: bool = False) -> List[int]:
         """Converts a string into a list of tokens."""
         assert isinstance(s, str)
-        t = self.sp_model.encode(s)
+        t = self.sp_model.EncodeAsIds(s)
         if bos:
             t = [self.bos_id] + t
         if eos:
@@ -43,4 +43,4 @@ class Tokenizer:
 
     def decode(self, t: List[int]) -> str:
         """Converts a list of tokens into a string."""
-        return self.sp_model.decode(t)
+        return self.sp_model.DecodeIds(t)
